5dc86467bdfe188cdbe489d664cd1777b7965041
Lynn Jongsma <kjongsma@cca.edu>
2017-09-08 14:35:06 -0700

    beginning the charette

---
+++ projects/charette/assets/script.js
@@ -0,0 +1,19 @@
+$(document).ready(function(){
+	
+	/*$('#button-3').click(function() {
+		$(this).animate({
+			opacity: 0.4,
+			marginLeft: '0.6in',
+			fontSize: '24px',
+			borderWidth: '10px'
+		}, 1000 );
+	});*/
+
+	$('#button-1').click(function() {
+		$(this).animate({
+			color: '#F500FF',
+			borderRadius: '100%'
+		})
+	})
+
+});
