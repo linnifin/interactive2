1ac001bfaed9aebc149590585ce1f25c690806b8
Lynn Jongsma <kjongsma@cca.edu>
2017-09-09 23:39:40 -0700

    its fuc k i ng done

--- projects/charette/assets/script.js
+++ projects/charette/assets/script.js
@@ -101,4 +101,208 @@ $(document).ready(function(){
 		})
 	});
 
+	$("#b25img").hide(0);
+
+	$("#button-25").click(function() {
+		$("#b25img").show(0).hide(4000);
+	});
+
+	$(".b26img").hide(0);
+
+	$("#button-26").click(function() {
+		$(".b26img").show(0);
+	});
+
+	$("#button-27").click(function() {
+		$(".b26img").animate({
+			top: "+=50px",
+		})
+	});
+
+	$("#button-28").click(function() {
+		$(".b26img").animate({
+			top: "100px",
+		},0);
+	});
+
+	$("#button-29").click(function() {
+		$(".b26img").animate({
+			maxHeight: "50px",
+		})
+	});
+
+	$("#button-30").click(function() {
+		$(".b26img").animate({
+			maxHeight: "200px",
+		})
+		.hide(10000);
+	});
+
+	$(".button-31b").hide(0);
+
+	$("#button-31").click(function() {
+		$(".button-31b").show(0);
+	});
+
+	$("#button-32").click(function() {
+		$(".item-row1").animate({
+			height: "-=2px",
+			width: "-=2px",
+		})
+	})
+
+	$("#button-32").click(function() {
+		$(".item-row2").animate({
+			height: "-=2px",
+			width: "-=2",
+		})
+	})
+
+	$("#button-32").click(function() {
+		$(".item-row3").animate({
+			height: "-=2px",
+			width: "-=2",
+		})
+	})
+
+	$("#button-32").click(function() {
+		$(".item-row4").animate({
+			height: "-=2px",
+			width: "-=2",
+		})
+	})
+
+	$("#button-32").click(function() {
+		$(".item-row5").animate({
+			height: "-=2px",
+			width: "-=2",
+		})
+	})
+
+	$("#button-34").click(function() {
+		$(".item-row1").animate({
+			height: "+=10px",
+		})
+	})
+
+	$("#button-34").click(function() {
+		$(".item-row2").animate({
+			height: "+=10px",
+		})
+	})
+
+	$("#button-34").click(function() {
+		$(".item-row3").animate({
+			height: "+=10px",
+		})
+	})
+
+	$("#button-34").click(function() {
+		$(".item-row4").animate({
+			height: "+=10px",
+		})
+	})
+
+	$("#button-34").click(function() {
+		$(".item-row5").animate({
+			height: "+=10px",
+		})
+	})
+
+	$("#button-36").click(function() {
+		$(".item-row1").animate({
+			height: "190px",
+			width: "190px",
+		},5000);
+	})
+
+	$("#button-36").click(function() {
+		$(".item-row2").animate({
+			height: "190px",
+			width: "190px",
+		},5000);
+	})
+
+	$("#button-36").click(function() {
+		$(".item-row3").animate({
+			height: "190px",
+			width: "190px",
+		},5000);
+	})
+
+	$("#button-36").click(function() {
+		$(".item-row4").animate({
+			height: "190px",
+			width: "190px",
+		},5000);
+	})
+
+	$("#button-36").click(function() {
+		$(".item-row5").animate({
+			height: "190px",
+			width: "190px",
+		},5000);
+	});
+
+	$("#button-37").click(function() {
+		$(".button-31b").animate({
+			left: "+=10px",
+		});
+	})
+
+	$("#b38img").hide();
+
+	$("#button-38").click(function() {
+		$("#b38img").show();
+	})
+
+	$("#p40").hide();
+
+	$("#button-40").click(function() {
+		$("#p40").show(2000).hide(2000);
+	});
+
+	$("#button-41").click(function() {
+		window.print();
+	})
+
+	$("#button-42").click(function() {
+		$(".button-31b").hide(1000);
+	})
+
+	$("#button-43").click(function() {
+		$(this).animate({
+			height: "-=1px",
+			width: "-=1px",
+		});
+	})
+
+	$("#button-44").click(function() {
+		document.getElementById("button-44").innerHTML = (Math.floor(Math.random() * 203));
+	});
+
+	$("#button-45").click(function() {
+		$("#button-44").animate({
+			marginTop: "+=20px",
+		})
+	});
+
+	$("#button-46").click(function() {
+		document.getElementById("button-46").innerHTML = "i ran out of ideas so the next buttons do nothing";
+	})
+
+	$("#button-48").click(function() {
+		document.getElementById("button-48").innerHTML = "nothing";
+	})
+
+	$("#b49img").hide();
+
+	$("#button-49").click(function() {
+		$("#b49img").toggle();
+	})
+
+	$("#button-50").click(function() {
+		document.write("nothing");
+	})
+
 });
