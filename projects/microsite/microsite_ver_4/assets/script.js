8b17a11b6ca9d80e24b202e0d302337472a242af
Lynn Jongsma <kjongsma@cca.edu>
2017-09-20 00:30:44 -0700

    yeah

---
+++ projects/microsite/microsite_ver_4/assets/script.js
@@ -0,0 +1,114 @@
+$(document).ready(function(){
+
+$('#a').click(function() {
+
+		$("#img1").animate({
+			top: "-185px",
+			},30000)
+		$("#img1").animate({
+			top: "100%",
+		}, 0)
+
+		});
+
+$('#b').click(function() {
+
+		$("#img2").animate({
+			top: "-185px",
+			},15000)
+		$("#img2").animate({
+			top: "100%",
+		}, 0)
+
+		});
+
+$('#c').click(function() {
+
+		$("#img3").animate({
+			top: "-185px",
+			},8000)
+		$("#img3").animate({
+			top: "100%",
+		}, 0)
+
+		});
+
+$('#d').click(function() {
+
+		$("#img4").animate({
+			top: "-185px",
+			},40000)
+		$("#img4").animate({
+			top: "100%",
+		}, 0)
+
+		});
+
+$('#e').click(function() {
+
+		$("#img5").animate({
+			top: "-185px",
+			},25000)
+		$("#img5").animate({
+			top: "100%",
+		}, 0)
+
+		});
+
+$('#f').click(function() {
+
+		$("#img6").animate({
+			top: "-185px",
+			},20000)
+		$("#img6").animate({
+			top: "100%",
+		}, 0)
+
+		});
+
+$('#g').click(function() {
+
+		$("#img7").animate({
+			top: "-185px",
+			},7000)
+		$("#img7").animate({
+			top: "100%",
+		}, 0)
+
+		});
+
+$('#h').click(function() {
+
+		$("#img8").animate({
+			top: "-185px",
+			},17000)
+		$("#img8").animate({
+			top: "100%",
+		}, 0)
+
+		});
+
+$('#i').click(function() {
+
+		$("#img9").animate({
+			top: "-185px",
+			},17000)
+		$("#img9").animate({
+			top: "100%",
+		}, 0)
+
+		});
+
+$('#j').click(function() {
+
+		$("#img10").animate({
+			top: "-185px",
+			},17000)
+		$("#img10").animate({
+			top: "100%",
+		}, 0)
+
+		});
+
+
+});
