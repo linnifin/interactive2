565d64f803cd88fb271fedcb1382793f2eb93084
Lynn Jongsma <kjongsma@cca.edu>
2017-09-19 23:13:36 -0700

    Microsite

---
+++ projects/microsite/assets/script.js
@@ -0,0 +1,71 @@
+$(document).ready(function(){
+
+$('#a').click(function() {
+
+		$("#img1").animate({
+			top: "-185px",
+			},30000)
+		$("#img1").animate({
+			top: "100%",
+		}, 0)
+
+		});
+
+$('#b').click(function() {
+
+		$("#img2").animate({
+			top: "-185px",
+			},15000)
+		$("#img2").animate({
+			top: "100%",
+		}, 0)
+
+		});
+
+$('#c').click(function() {
+
+		$("#img3").animate({
+			top: "-185px",
+			},8000)
+		$("#img3").animate({
+			top: "100%",
+		}, 0)
+
+		});
+
+$('#d').click(function() {
+
+		$("#img4").animate({
+			top: "-185px",
+			},40000)
+		$("#img4").animate({
+			top: "100%",
+		}, 0)
+
+		});
+
+$('#e').click(function() {
+
+		$("#img5").animate({
+			top: "-185px",
+			},25000)
+		$("#img5").animate({
+			top: "100%",
+		}, 0)
+
+		});
+
+$('#f').click(function() {
+
+		$("#img6").animate({
+			top: "-185px",
+			},25000)
+		$("#img6").animate({
+			top: "100%",
+		}, 0)
+
+		});
+
+
+
+});
