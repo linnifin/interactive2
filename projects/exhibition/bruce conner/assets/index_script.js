3d10ce95bdde3d0a5af809b2b62eab2c08ba14b4
Lynn Jongsma <kjongsma@cca.edu>
2017-11-20 00:47:19 -0800

    ggg

---
+++ projects/exhibition/bruce conner/assets/index_script.js
@@ -0,0 +1,59 @@
+$(document).ready(function(){
+
+$(".line1").hide();
+
+$(".line2").hide();
+
+$(".line3").hide();
+
+$(".line4").hide();
+
+$(".line5").hide();
+
+$("#error").hide();
+
+$("#thepage").hide();
+
+$("#home").hide();
+
+$("#portrait").hide();
+
+$("#assembled1").hide();
+
+$("#couch").hide();
+});
+
+
+$(window).load(function() {
+	$(".line1").show(6000);
+
+	$(".line2").show(6000);
+
+	$(".line3").show(6000);
+
+	$(".line4").show(6000);
+
+	$(".line5").show(6000);
+
+	$("#error").fadeIn(3000);
+
+	$("#thepage").fadeIn(3000);
+
+	$("#home").fadeIn(3000);
+
+	$("#portrait").fadeIn(3000);
+
+	$("#assembled1").fadeIn(3000);
+
+	$("#couch").fadeIn(3000);
+
+
+	});
+
+
+//end
+
+
+
+
+
